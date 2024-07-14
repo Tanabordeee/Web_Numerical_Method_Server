6d99da183ec21a0f8bef0cbf24723668bb3352fc
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2024-07-14 11:43:57 +0700

    connect database

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const NumericalRoute = require('./Routes/NumericalRoute');
 const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient()
 require("dotenv").config();
 const app = express();
 app.use(express.json());
@@ -12,6 +13,19 @@ app.use(cors({
     credentials: true,
 }));
 app.use(morgan("dev"));
+async function main() {
+    const allUsers = await prisma.user.findMany()
+    console.log(allUsers)
+    }
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
 app.use("/api" , NumericalRoute);
 app.listen(process.env.PORT , ()=>{
     console.log("listening on port " + process.env.PORT);

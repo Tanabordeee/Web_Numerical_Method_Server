e96f9a5b33886a4e7bfa8bf87f3e045a875266d4
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2024-07-17 00:10:13 +0700

    add create Equation

--- Routes/NumericalRoute.js
+++ Routes/NumericalRoute.js
@@ -4,11 +4,12 @@ const {Graphical_Method} = require("../controllers/Graphical_Method");
 const {Bisection_Method} = require("../controllers/Bisection_Method");
 const {FalsePosition_Method} = require("../controllers/FalsePosition_Method");
 const {Register , Login , requireLogin} = require("../controllers/Login_Register");
+const {AddEquation} = require("../controllers/AddEquation");
 router.post("/Register" , Register);
 router.post("/Login" , Login);
-
 router.use(requireLogin);
 router.post("/Graphical" , Graphical_Method);
 router.post("/Bisection" , Bisection_Method);
 router.post("/FalsePosition" , FalsePosition_Method);
+router.post("/AddEquation" , AddEquation);
 module.exports = router;
---
+++ controllers/AddEquation.js
@@ -0,0 +1,15 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+exports.AddEquation = async(req ,res) =>{
+    const { equation } = req.body;
+    try{
+        const equation_admin = await prisma.Question.create({
+            data: {
+                equation : equation
+            }
+        });
+        return res.status(200).json({ "Message": "equation created successfully" });
+    }catch(err){
+        return res.status(400).json({ "Message": err.message });
+    }
+}

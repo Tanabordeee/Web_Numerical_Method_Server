7c38803a4603892c51fcc53d897487b68ccb427e
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2024-11-01 19:17:01 +0700

    update swagger

--- index.js
+++ index.js
@@ -6,22 +6,23 @@ const NumericalRoute = require('./Routes/NumericalRoute');
 require('dotenv').config();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
+const swaggerUiDist = require('swagger-ui-dist');
 
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-    origin: '*',
-    credentials: true,
-}));
+app.use(cors({ origin: '*', credentials: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
+// Serve Swagger UI files manually
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/swagger-ui', express.static(swaggerUiDist.getAbsoluteFSPath()));
+
 app.use('/api', NumericalRoute);
 
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });

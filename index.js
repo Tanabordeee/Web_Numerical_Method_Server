90568bb713f1ebb13236bd1945a0ccfdf358a804
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2025-03-19 15:16:18 +0700

    add migration

--- index.js
+++ index.js
@@ -7,8 +7,22 @@ require('dotenv').config();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 const swaggerUiDist = require('swagger-ui-dist');
+const { execSync } = require('child_process');
 
 const app = express();
+async function applyMigration() {
+  try {
+    console.log('🔄 Checking and applying migrations...');
+
+    execSync('npx prisma migrate deploy', { stdio: 'inherit' });
+    console.log('✅ Database schema synced successfully');
+  } catch (error) {
+    console.error('❌ Error syncing database schema:', error);
+  }
+}
+
+
+applyMigration();
 
 app.use(express.json());
 app.use(cors({ origin: 'https://numerical-method-project.vercel.app/', credentials: true }));
@@ -20,9 +34,10 @@ app.use(morgan('dev'));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/swagger-ui', express.static(swaggerUiDist.getAbsoluteFSPath()));
 
+
 app.use('/api', NumericalRoute);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+  console.log(`Listening on port ${PORT}`);
 });
